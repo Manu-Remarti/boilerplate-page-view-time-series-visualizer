fe97b005789aeeac63e79e1ecade1907f0451d5d
Manu-Remarti <lmanuel.reyes88@gmail.com>
2025-07-08 13:58:28 -0600

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,11 +5,14 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates['date'], index_col='date')
 
 # Clean data
-df = None
 
+low= df['value'].quantile(0.025)
+high= df['value'].quantile(0.975)
+
+df = df[(df['value'] >= low) & (df['value'] <= high)]
 
 def draw_line_plot():
     # Draw line plot
